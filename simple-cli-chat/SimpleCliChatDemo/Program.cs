59159b339d91fcf47be0bd4a60cf19bbe3fa12fc
Michał Dudniczenko <m.dudniczenko@godeltech.com>
2026-05-21 12:53:06 +0200

    Add basics cli chat demo

---
+++ simple-cli-chat/SimpleCliChatDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Agents.AI;
+using OpenAI;
+using OpenAI.Chat;
+using System.ClientModel;
+
+const string modelName = "granite4.1:3b";
+const string apiKey = "YOUR-API-KEY";
+const string apiUrl = "http://172.19.96.1:11434/v1";
+
+IChatClient chatClient =
+    new ChatClient(
+        model: modelName,
+        credential: new ApiKeyCredential(apiKey),
+        options: new OpenAIClientOptions
+        {
+            Endpoint = new Uri(apiUrl)
+        })
+    .AsIChatClient();
+
+AIAgent agent = chatClient.AsAIAgent();
+
+Console.Write("\n=======================================================\n\n");
+
+while (true)
+{
+    Console.Write("PROMPT:\t\t");
+
+    var prompt = string.Empty;
+    while (string.IsNullOrWhiteSpace(prompt))
+    {
+        prompt = Console.ReadLine();
+    }
+
+    Console.Write("RESPONSE:\t");
+
+    await foreach (var update in agent.RunStreamingAsync(prompt))
+    {
+        Console.Write(update);
+    }
+
+    Console.Write("\n\n=======================================================\n\n");
+}
